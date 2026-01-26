d70939343fce63ea5c3c134c6a7227d5cd70c219
hlcxpl <hlcxpl@gmail.com>
2026-01-26 17:52:08 -0300

    docs: documentación con Swagger

---
+++ src/docs/swagger.js
@@ -0,0 +1,38 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Convenios Pullman',
+      version: '1.0.0',
+      description: 'Documentación del backend de convenios Pullman'
+    },
+    servers: [
+      {
+        url: 'http://localhost:3030/api',
+        description: 'Servidor local'
+      }
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT'
+        }
+      }
+    },
+    security: [
+      {
+        bearerAuth: []
+      }
+    ]
+  },
+  apis: [
+    './src/routes/*.js',
+    './src/controllers/*.js'
+  ]
+};
+
+module.exports = swaggerJSDoc(options);
