690e8424d1357629334056822daf629ff0d52adb
hlcxpl <hlcxpl@gmail.com>
2026-03-11 12:24:02 -0300

    chore: eliminar archivos de prueba y temporales del repositorio

--- check_pm2.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  console.log(execSync('pm2 status', { encoding: 'utf8' }));
-  console.log(execSync('pm2 logs --lines 50', { encoding: 'utf8' }));
-} catch(e) {
-  console.log("PM2 Error:", e.message);
-}
